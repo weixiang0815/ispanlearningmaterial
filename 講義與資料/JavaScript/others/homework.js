980e8348bc4885f0ed910d3c89eecb839cf97daa
Henry Wang <henry940129@gmail.com>
2022-12-01 20:43:38 +0800

    changed

--- 講義與資料/JavaScript/others/homework.js
+++ 講義與資料/JavaScript/others/homework.js
@@ -1,9 +1,9 @@
 document.write(`<div class="body">`);
 document.write(`<h2>第一題</h2>`);
 document.write(`<table><tr>`);
-for(let i = 1; i <= 9; i ++){
+for (let i = 1; i <= 9; i++) {
     document.write(`<td>`);
-    for(let j = 2; j <= 9; j ++){
+    for (let j = 2; j <= 9; j++) {
         document.write(`<p>${j} × ${i} = ${i * j}</p>`);
     }
     document.write(`</td>`);
@@ -13,6 +13,63 @@ document.write(`</div>`);
 
 document.write(`<div class="body">`);
 document.write(`<h2>第二題</h2>`);
+document.write(`<form>`);
+document.write(`<fieldset>`);
+document.write(`<legend>Form Check</legend>`);
+// 姓名
+document.write(`<div class=\"divform\">`);
+document.write(`<label>姓名：</label>`);
+document.write(`<input class=\"name\" onblur=\"checkName()\">`);
+document.write(`<span id=\"name\"></span>`);
+document.write(`<p>（1.不可空白 2.至少兩個字以上 3.必須全部為中文字）</p>`);
+document.write(`</div>`);
+let checkName = () => {
+    let myName = document.getElementsByClassName("name")[0];
+    let check = document.getElementById("name");
+    let regex = /^[\u4E00-\u9FFF]{2,}$/gm;
+    if (regex.exec(myName.value) == null) {
+        document.getElementById("name").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+    } else {
+        document.getElementById("name").innerHTML = '<i class="fa-regular fa-circle"></i>驗證成功';
+    }
+};
+// 密碼
+document.write(`<div class=\"divform\">`);
+document.write(`<label>密碼：</label>`);
+document.write(`<input class=\"pw\" onblur=\"checkPW()\">`);
+document.write(`<span id=\"pw\"></span>`);
+document.write(`<p>（1.不可空白 2.至少6個字且必須包含英數字、特殊字元[!@#$%^&*]）</p>`);
+document.write(`</div>`);
+let checkPW = () => {
+    let myPW = document.getElementsByClassName("pw")[0];
+    let check = document.getElementById("pw");
+    let regex = /^[\u4E00-\u9FFF]{2,}$/gm;
+    if (regex.exec(myPW.value) == null) {
+        document.getElementById("pw").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+    } else {
+        document.getElementById("pw").innerHTML = '<i class="fa-regular fa-circle"></i>驗證成功';
+    }
+};
+// 日期
+document.write(`<div class=\"divform\">`);
+document.write(`<label>日期：</label>`);
+document.write(`<input class=\"date\" onblur=\"checkDate()\">`);
+document.write(`<span id=\"date\"></span>`);
+document.write(`<p>格式:西元年/月/日（yyyy/mm/dd）</p>`);
+document.write(`</div>`);
+let checkDate = () => {
+    let myDate = document.getElementsByClassName("date")[0];
+    let check = document.getElementById("date");
+    let regex = /^[\u4E00-\u9FFF]{2,}$/gm;
+    if (regex.exec(myDate.value) == null) {
+        document.getElementById("date").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+    } else {
+        document.getElementById("date").innerHTML = '<i class="fa-regular fa-circle"></i>驗證成功';
+    }
+};
+
+document.write(`</fieldset>`);
+document.write(`</form>`);
 document.write(`</div>`);
 
 document.write(`<div class="body">`);
