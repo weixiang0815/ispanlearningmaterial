baf30607ef4920148c28499e6819722fe5f9d880
Henry Wang <henry940129@gmail.com>
2022-12-01 14:22:40 +0800

    changed

--- 講義與資料/JavaScript/others/homework.js
+++ 講義與資料/JavaScript/others/homework.js
@@ -1,10 +1,12 @@
 document.write(`<h2>第一題</h2>`);
 document.write(`<table>`);
+document.write(`<tr>`);
 for(let i = 1; i <= 9; i ++){
-    document.write(`<tr>`);
+    document.write(`<td>`);
     for(let j = 2; j <= 9; j ++){
-        document.write(`<td>${j} × ${i} = ${i * j}</td>`);
+        document.write(`<p>${j} × ${i} = ${i * j}</p>`);
     }
-    document.write(`</tr>`);
+    document.write(`</td>`);
 }
+document.write(`</tr>`);
 document.write(`</table>`);
