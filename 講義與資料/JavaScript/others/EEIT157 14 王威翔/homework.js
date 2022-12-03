a659de8002fe8391c15477f2dfd6829ffb9d3808
王威翔 （Henry Wang） <henry940129@gmail.com>
2022-12-03 19:35:32 +0800

    modified

--- 講義與資料/JavaScript/others/EEIT157 14 王威翔/homework.js
+++ 講義與資料/JavaScript/others/EEIT157 14 王威翔/homework.js
@@ -3,11 +3,11 @@ document.write(`<div class="body">`);
 document.write(`<h2>第一題</h2>`);
 document.write(`<table><tr>`);
 for (i = 2; i <= 9; i++) {
-    document.write(`<td>`);
-    for (j = 1; j <= 9; j++) {
-        document.write(`<p>${i} × ${j} = ${i * j}</p>`);
-    }
-    document.write(`</td>`);
+  document.write(`<td>`);
+  for (j = 1; j <= 9; j++) {
+    document.write(`<p>${i} × ${j} = ${i * j}</p>`);
+  }
+  document.write(`</td>`);
 }
 document.write(`</tr></table>`);
 document.write(`</div>`);
@@ -26,29 +26,35 @@ document.write(`<span id=\"name\"></span>`);
 document.write(`<p>（1.不可空白 2.至少兩個字以上 3.必須全部為中文字）</p>`);
 document.write(`</div>`);
 document.getElementsByClassName("name")[0].addEventListener("blur", () => {
-    let myName = document.getElementsByClassName("name")[0];
-    let regex = /^[\u4E00-\u9FFF]{2,}$/;
-    if (regex.exec(myName.value) == null) {
-        document.getElementById("name").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
-    } else {
-        document.getElementById("name").innerHTML = '<i class="fa-regular fa-circle"></i>驗證成功';
-    }
+  let myName = document.getElementsByClassName("name")[0];
+  let regex = /^[\u4E00-\u9FFF]{2,}$/;
+  if (regex.exec(myName.value) == null) {
+    document.getElementById("name").innerHTML =
+      '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+  } else {
+    document.getElementById("name").innerHTML =
+      '<i class="fa-regular fa-circle"></i>驗證成功';
+  }
 });
 // 密碼
 document.write(`<div class=\"divform\">`);
 document.write(`<label>密碼：</label>`);
 document.write(`<input class=\"pw\">`);
 document.write(`<span id=\"pw\"></span>`);
-document.write(`<p>（1.不可空白 2.至少6個字且必須包含英數字、特殊字元[!@#$%^&*]）</p>`);
+document.write(
+  `<p>（1.不可空白 2.至少6個字且必須包含英數字、特殊字元[!@#$%^&*]）</p>`
+);
 document.write(`</div>`);
 document.getElementsByClassName("pw")[0].addEventListener("blur", () => {
-    let myPW = document.getElementsByClassName("pw")[0];
-    let regex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$^&*])[a-zA-Z\d!@#$^&*]{6,}$/;
-    if (regex.exec(myPW.value) == null) {
-        document.getElementById("pw").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
-    } else {
-        document.getElementById("pw").innerHTML = '<i class="fa-regular fa-circle"></i>驗證成功';
-    }
+  let myPW = document.getElementsByClassName("pw")[0];
+  let regex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$^&*])[a-zA-Z\d!@#$^&*]{6,}$/;
+  if (regex.exec(myPW.value) == null) {
+    document.getElementById("pw").innerHTML =
+      '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+  } else {
+    document.getElementById("pw").innerHTML =
+      '<i class="fa-regular fa-circle"></i>驗證成功';
+  }
 });
 // 日期
 document.write(`<div class=\"divform\">`);
@@ -58,22 +64,25 @@ document.write(`<span id=\"date\"></span>`);
 document.write(`<p>格式:西元年/月/日（yyyy/mm/dd）</p>`);
 document.write(`</div>`);
 document.getElementsByClassName("date")[0].addEventListener("blur", () => {
-    let myDate = document.getElementsByClassName("date")[0];
-    let regex = /^\d{4}\/\d{2}\/\d{2}$/;
-    if (regex.exec(myDate.value) == null) {
-        document.getElementById("date").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+  let myDate = document.getElementsByClassName("date")[0];
+  let regex = /^\d{4}\/\d{2}\/\d{2}$/;
+  if (regex.exec(myDate.value) == null) {
+    document.getElementById("date").innerHTML =
+      '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
+  } else {
+    let d = new Date(myDate.value);
+    let month =
+      d.getMonth() + 1 >= 10 ? d.getMonth() + 1 : "0" + (d.getMonth() + 1);
+    let day = d.getDate() >= 10 ? d.getDate() : "0" + d.getDate();
+    let date = d.getFullYear() + "/" + month + "/" + day;
+    if (date != myDate.value) {
+      document.getElementById("date").innerHTML =
+        '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
     } else {
-        let d = new Date(myDate.value);
-        let month = d.getMonth() + 1 >= 10 ? d.getMonth() + 1 : "0" + (d.getMonth() + 1);
-        let day = d.getDate() >= 10 ? d.getDate() : "0" + d.getDate();
-        let date = d.getFullYear() + "/" + month + "/" + day;
-        if (date != myDate.value) {
-            document.getElementById("date").innerHTML = '<i class="fa-solid fa-xmark"></i>驗證失敗，請符合規則';
-        }
-        else {
-            document.getElementById("date").innerHTML = '<i class="fa-regular fa-circle"></i>驗證成功';
-        }
+      document.getElementById("date").innerHTML =
+        '<i class="fa-regular fa-circle"></i>驗證成功';
     }
+  }
 });
 document.write(`</fieldset>`);
 document.write(`</form>`);
@@ -84,11 +93,29 @@ document.write(`<div class="body">`);
 document.write(`<h2>第三題</h2>`);
 document.write(`<div class=\"stars\">`);
 for (i = 1; i <= 5; i++) {
-    document.write(`<a><img src=\"Images/chngstar.gif\" /></a>`);
+  document.write(`<a><img src=\"Images/chngstar.gif\" /></a>`);
 }
-
-document.write(`</div>`)
-document.write(`<div class=\"text\">`)
+const starWrapper = document.querySelector(".stars");
+const stars = document.querySelectorAll(".stars a");
+stars.forEach((starClicked, clickedIndex) => {
+  starClicked.addEventListener("click", () => {
+    starWrapper.classList.add("disabled");
+    stars.forEach((otherStars, otherIndex) => {
+      if (otherIndex <= clickedIndex) {
+        otherStars.classList.add("active");
+      }
+    });
+  });
+  //   console.log(`你按了第${clickedIndex + 1}顆星`);
+  starClicked.addEventListener("dblclick", () => {
+    starWrapper.classList.remove("disabled");
+    stars.forEach((otherStars) => {
+        otherStars.classList.remove("active");
+    })
+  });
+});
+document.write(`</div>`);
+document.write(`<div class=\"text\">`);
 document.write(`<h3>點一下星星可評分，點兩下可重置</h3>`);
 document.write(`<h3 id=\"stars\">評分為....</h3>`);
 document.write(`</div>`);
