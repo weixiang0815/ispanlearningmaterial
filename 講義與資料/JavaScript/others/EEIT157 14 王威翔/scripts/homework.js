fed6f10c8bac1bc9fd0d825e7bae2c8b215567de
Henry Wang <henry940129@gmail.com>
2022-12-07 12:51:45 +0800

    modified

---
+++ 講義與資料/JavaScript/others/EEIT157 14 王威翔/scripts/homework.js
@@ -0,0 +1,5 @@
+document.write(`<script src="scripts/第一題.js"></script>`);
+document.write(`<script src="scripts/第二題.js"></script>`);
+document.write(`<script src="scripts/第三題.js"></script>`);
+document.write(`<script src="scripts/第四題.js"></script>`);
+document.write(`<script src="scripts/第五題.js"></script>`);
