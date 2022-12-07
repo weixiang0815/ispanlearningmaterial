582c976710ac3e130e4382d9cbc2730afd2e9313
Henry Wang <henry940129@gmail.com>
2022-12-07 12:55:27 +0800

    modified

--- 講義與資料/JavaScript/others/EEIT157 14 王威翔/scripts/homework.js
+++ 講義與資料/JavaScript/others/EEIT157 14 王威翔/scripts/homework.js
@@ -1,7 +1,3 @@
 for (let i = 1; i <= 5; i++) {
   document.write(`<script src="scripts/第${i}題.js"></script>`);
 }
-// document.write(`<script src="scripts/第二題.js"></script>`);
-// document.write(`<script src="scripts/第三題.js"></script>`);
-// document.write(`<script src="scripts/第四題.js"></script>`);
-// document.write(`<script src="scripts/第五題.js"></script>`);
